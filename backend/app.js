74558f9f2e3b09f6267a6e3c11ed3cfee4d773a0
Vijaykumarjntu <jntuvijaykumar@gmail.com>
2026-03-02 20:49:00 +0530

    rectified cors issues

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
+
+
+app.use(express.json())
+app.use(cors())
+const products = [
+  {
+    "id": "P001",
+    "name": "Laptop",
+    "category": "Electronics",
+    "price": 999.99,
+    "description": "A high-performance laptop for work and gaming."
+  },
+  {
+    "id": "P002",
+    "name": "Smartphone",
+    "category": "Electronics",
+    "price": 499.99,
+    "description": "A budget-friendly smartphone with great features."
+  }
+]
 
 app.get("/api/products",async(req,res)=>{
-    return res.json("this is the products")
+    return res.json(products)
 })
 
-app.listen(3000,()=>{
+app.listen(5000,()=>{
     console.log("server started running")
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [products,setProducts] = useState([])
+  
+  useEffect(()=>{
+    const fetchProducts = async()=>{
+      const res = await axios.get('http://localhost:5000/api/products')
+      setProducts(res)
+    }
+    fetchProducts()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map(x => (
+        <div className='prod-item'>
+          <h2>{x.title}</h2>
+          <p>{x.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
